00abfa5870d166fb01ebcbb027a340597ae1ba72
Tom Ronen <mystic74@gmail.com>
2018-11-04 20:06:25 +0200

    project_files : adding the files for clion

---
+++ cmake-build-debug/CMakeFiles/ass1.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tom/projects/ass1/Main.cpp" "/home/tom/projects/ass1/cmake-build-debug/CMakeFiles/ass1.dir/Main.cpp.o"
+  "/home/tom/projects/ass1/src/Restaurant.cpp" "/home/tom/projects/ass1/cmake-build-debug/CMakeFiles/ass1.dir/src/Restaurant.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
