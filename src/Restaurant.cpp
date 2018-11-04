78f2cd5cb2c6eec7447947fab5c3028958be9f47
Tom Ronen <mystic74@gmail.com>
2018-11-04 20:06:47 +0200

    restaurant.cpp : adding a basic function

---
+++ src/Restaurant.cpp
@@ -0,0 +1,15 @@
+//
+// Created by tom on 03/11/18.
+//
+
+#include "../include/Restaurant.h"
+#include <cstdio>
+#include <iostream>
+
+void Restaurant::start()
+{
+    std::cout << "Resturant is now open!" << std::endl;
+
+}
+
+
