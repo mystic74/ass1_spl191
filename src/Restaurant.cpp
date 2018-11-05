b04b10b7efc211fdf9d0d45fd658b613ceccb841
Tom Ronen <mystic74@gmail.com>
2018-11-05 08:41:48 +0200

    stub_implementation: for restaurant.cpp
    still WIP, just throwing exceptions for now for most of them

--- src/Restaurant.cpp
+++ src/Restaurant.cpp
@@ -6,10 +6,46 @@
 #include <cstdio>
 #include <iostream>
 
+Restaurant::Restaurant()
+{
+    throw std::runtime_error("error");
+}
+
+Restaurant::Restaurant(const std::string &configFilePath)
+{
+    throw std::runtime_error("error");
+}
+
+
+const std::vector<BaseAction*>& Restaurant::getActionsLog() const
+{
+    throw std::runtime_error("Not implemented");
+    std::vector<BaseAction*> a =  new std::vector<BaseAction*>;
+
+    return &a;
+}
+
+int Restaurant::getNumOfTables() const
+{
+    throw std::runtime_error("error");
+    return 1;
+}
+
+Table* Restaurant::getTable(int ind)
+{
+    throw std::runtime_error("error");
+    return std::nullptr;
+}
+
+std::vector<Dish>& Restaurant::getMenu()
+{
+    throw std::runtime_error("error");
+    return std::nullptr;
+}
+
 void Restaurant::start()
 {
     std::cout << "Resturant is now open!" << std::endl;
-
 }
 
 
